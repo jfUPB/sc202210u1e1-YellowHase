6ffee57e7b138c3841e19233349f38a560780e7b
YellowHase <valelosa@gmail.com>
2022-02-11 00:18:15 -0500

    PrimerPunto

--- problem/program.asm
+++ problem/program.asm
@@ -2,49 +2,7 @@
 // 000429546
 // VALENTINA.LONDONOSA@UPB.EDU.CO
 (START)
-@16384
-D=A
-@1 
-M=D
-//Inicio de la pantalla
-@1
-D=M
-@2 
-M=D
-@70
-D=M
-@3
-M=D
-//Ciclo -1
-@3
-D=M
-@24575
-D=D-A
-@4
-D;JGT
-@2
-//Se le da el valor -1 desde el inicio de la pantalla 
-A=M
-M=-1
-@2
-M=M+1
-@12
-0;JMP //Fin del ciclo 
-@2
-D=M 
-@24575
-D=D-A
-@4
-D;JGT
-@2
-A=M 
-M=0
-@2
-M=M+1
-@25
-0;JMP
-
-
+//Pantalla en negro con F
 @16384
 D=A
 @1 
@@ -64,12 +22,11 @@ D=M
 @24576
 A=M
 M=D-A
-
-
+M;JGT
+@21
+M;JEQ
 @24575
 D=D-A
-@4
-D;JGT
 @2
 //Se le da el valor -1 desde el inicio de la pantalla 
 A=M
@@ -78,17 +35,4 @@ M=-1
 M=M+1
 @12
 0;JMP //Fin del ciclo 
-@2
-D=M 
-@24575
-D=D-A
-@4
-D;JGT
-@2
-A=M 
-M=0
-@2
-M=M+1
-@25
-0;JMP
- 
+//Fin pantalla en negro con F
