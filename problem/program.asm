ee7da57deba87cba9c46fca027035ef9ae9f56b5
YellowHase <valelosa@gmail.com>
2022-02-11 22:11:23 -0500

    PrimerPuntoCorregido

--- problem/program.asm
+++ problem/program.asm
@@ -12,6 +12,7 @@ M=D
 D=M
 @2 
 M=D
+//Guardar el 70 en la posicion 3
 @70
 D=A
 @3
@@ -23,9 +24,17 @@ D=M
 A=M
 M=D-A
 M;JGT
-@21
+@20
 M;JEQ
+//Limites pantalla
+@16384
+D=M
 @24575
+D=M
+D=D-A
+@5
+D;JGT
+//Ciclo pintar a negro
 D=D-A
 @2
 //Se le da el valor -1 desde el inicio de la pantalla 
@@ -33,6 +42,6 @@ A=M
 M=-1
 @2
 M=M+1
-@12
+@21
 0;JMP //Fin del ciclo 
 //Fin pantalla en negro con F
